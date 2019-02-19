bbb0fcc920f8497f864a6da87d1f5d3600378686
Ryan <ryangeorgebarker0@gmail.com>
2019-02-19 11:37:19 +0000

    clean up for development start

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
 console.log('Starting App.js')
 
 const fs = require('fs')
-const os = require('os')
 const _ = require('lodash')
+
 const notes = require('./notes')
-
-// console.log(_.isString(true))
-// console.log(_.isString('A name'))
-
-var filteredArray = _.uniq(['Ryan', 1, 'Ryan', 1, 2, 3, 4])
-
-console.log(filteredArray)
-
-// var res = notes.addNote(5, 4)
-// console.log(res)
-
-// var user = os.userInfo()
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
-//     if (err) {
-//         console.log('Unable to write to file')
-//     }
-// })
